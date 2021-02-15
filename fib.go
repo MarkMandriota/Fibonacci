d05462dff49dc5b78dbf57a207f84a868dee3a8b
Nameless Name <62650188+MarkMandriota@users.noreply.github.com>
2021-02-15 17:25:26 +0100

    Update fib.go

--- fib.go
+++ fib.go
@@ -3,12 +3,3 @@ package fib
 //go:noinline
 // Fib - returns the fibonacci number by ordinal
 func Fib(n uint64) uint64
-
-func New() func() uint64 {
-	var x, y uint64 = 0, 1
-
-	return func() uint64 {
-		x, y = x+y, x
-		return y
-	}
-}
